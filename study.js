bd5c45e727e13993c8996ecae57af30af8b08004
minseokLim <mslim8803@naver.com>
2021-03-26 20:44:05 +0900

    First Commit

---
+++ study.js
@@ -0,0 +1,32 @@
+class Human {
+    constructor(type = 'human') {
+        this.type = type;
+    }
+
+    static isHuman(human) {
+        return human instanceof Human;
+    }
+
+    breathe() {
+        alert('ahahah');
+    }
+}
+
+class Zero extends Human {
+    constructor(type, firstName, lastName) {
+        super(type);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    sayName() {
+        super.breathe();
+        alert(`${this.firstName} ${this.lastName}`);
+    }
+}
+
+const newZero = new Zero('human', 'Zero', 'Cho');
+
+const result = Human.isHuman(newZero);
+
+console.log(result)
